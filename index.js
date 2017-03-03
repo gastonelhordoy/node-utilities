32715a771d034e5a8a3b230bae4080a87231e8c7
Gaston <gaston.elhordoy@gmail.com>
2017-03-03 12:27:24 -0300

    Include lodash-deep dependency and extend lodash

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 'use strict';
 
+// extend lodash with lodash-deep mixins
+require('lodash').mixin(require('lodash-deep'));
+
+// instantiate all the utilities
 module.exports = require('./libraries');
