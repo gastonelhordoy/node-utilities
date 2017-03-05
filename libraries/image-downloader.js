e3f64fe0d8825a0a7c8dadb7ddf4d6aca6f0ce5c
Gaston <gaston.elhordoy@gmail.com>
2017-03-05 13:35:49 -0300

    Add caching support to image-downloader

--- libraries/image-downloader.js
+++ libraries/image-downloader.js
@@ -9,34 +9,60 @@
 
 
 // const base64 = require('base64-stream');
+
+const _ = require('lodash');
 const Promise = require('bluebird');
+const NodeCache = require('node-cache');
 let request = require('request');
 
 request = Promise.promisifyAll(request.defaults({ encoding: null }), { multiArgs: true });
 
 
-module.exports = function imageDownloader(imgUrl, options) {
-  options = options || {};
+module.exports = function buildImageDownloader(options) {
+  options = _.defaults(options || {}, {
+    stdTTL: 60 * 60 * 24, // one day
+    checkperiod: 60 * 60 // check every hour
+  });
 
-  return request.getAsync(imgUrl)
-  .spread((res, body) => {
-    let content = new Buffer(body);
-    if (!options.asBuffer) {
-      content = content.toString('base64');
+  // Configure cache for images
+  const IMAGES_CACHE = new NodeCache({
+    stdTTL: options.stdTTL,
+    checkperiod: options.checkperiod,
+    useClones: false // no need the overhead of cloning
+  });
+  IMAGES_CACHE.on('del', function(key) {
+    console.info('IMAGES CACHE: removing file:', key);
+  });
+  process.on('exit', function(code) {
+    console.info('IMAGES CACHE: closing before exiting:', code);
+    IMAGES_CACHE.close();
+  });
+
+  return function imageDownloader(imgUrl, options) {
+    // defaults
+    options = options || {};
+    options.asBuffer = options.asBuffer || false;
+
+    // cache short circuit
+    const cacheKey = imgUrl + '|\\|' + options.asBuffer;
+    let image = IMAGES_CACHE.get(cacheKey);
+    if (image) {
+      return Promise.resolve(image);
     }
 
-    return {
-      type: res.headers['content-type'],
-      content: content
-    };
-  });
-  // return new Promise(function (resolve, reject) {
+    return request.getAsync(imgUrl)
+    .spread((res, body) => {
+      let content = new Buffer(body);
+      if (!options.asBuffer) {
+        content = content.toString('base64');
+      }
 
-  //   http.get(imgUrl, function(res) {
-  //     if (res.statusCode === 200) {
-  //       res.pipe(base64.encode());
-  //     }
-  //   });
-
-  // });
+      image = {
+        type: res.headers['content-type'],
+        content: content
+      };
+      IMAGES_CACHE.set(cacheKey, image);
+      return image;
+    });
+  };
 };
