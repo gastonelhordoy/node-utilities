19656a353a6b2bd11482eb2e338ab83f3c661ba1
Gaston <gaston.elhordoy@gmail.com>
2017-03-14 12:44:26 -0300

    Add email-verifier utility which doesn’t only checks format but also DNS configuration

---
+++ libraries/email-verifier.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const isemail = require('isemail');
+
+module.exports = function emailVerifier(email) {
+  return new Promise(resolve => {
+    isemail.validate(email, { checkDNS: true }, resolve);
+  });
+};
